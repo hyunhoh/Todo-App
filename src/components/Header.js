72b580dccf007ff1b080dcefd3f86fb6a1aa1e0d
hyunhoh <skins0329@gmail.com>
2023-01-10 22:54:22 +0900

    make header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,30 @@
-import Nav from './Nav';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const HeaderContainer = styled.header`
+  background-color: aliceblue;
+  height: 10%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 const Header = () => {
-  return <Nav></Nav>;
+  return (
+    <>
+      <HeaderContainer>
+        <span>Todo App</span>
+        <nav>
+          <Link to="/">
+            <span>Home</span>
+          </Link>
+          <Link to="/todo">
+            <span>Todo</span>
+          </Link>
+        </nav>
+      </HeaderContainer>
+    </>
+  );
 };
 
 export default Header;
